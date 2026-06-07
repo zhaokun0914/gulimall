f6fbd062c15d67b7251c533744c94b0ac78eefca
zhaokun <283798272@qq.com>
2026-06-07 22:52:53 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.product;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GulimallProductApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/atguigu/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.ware;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GulimallWareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
